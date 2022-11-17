9f1b4cd118c99a68eadab36c1bc0ecd38dfaf61b
LiamTyl78 <114972385+LiamTyl78@users.noreply.github.com>
2022-11-17 16:34:50 -0500

    Add files via upload

---
+++ HighLow.java
@@ -0,0 +1,110 @@
+//Liam Tyler
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.UIManager;
+
+public class HighLow {
+    private int m_gamesPlayed = 0;
+    private int m_sumOfScores = 0;
+    static JFrame f;
+    private Deck deck;
+    
+    public static void main(String[] args) throws Exception {
+        HighLow highlow = new HighLow();
+        GHighLow p = new GHighLow();
+        highlow.play();
+        }
+    public HighLow(){
+        this.deck = new Deck();
+    }
+    public void play() {
+        boolean playing = true;
+        while (playing) {
+            m_sumOfScores =+ PlayARound();
+            char answer = PlayAgainPromt();
+            if (answer == 'n') {
+                playing = false;
+            }
+        }
+        DisplayFinalStats();
+    }
+
+    protected int PlayARound() {
+        m_gamesPlayed++;
+        int CorretGuesses = 0;
+        deck.Shuffle();
+        boolean playing = true;
+        Card NextCard;
+        Card CurrentCard;
+        CurrentCard = deck.DealCard();
+
+        while (playing) {
+            DisplayCurrentCard(CurrentCard);
+            char ans = GuessPrompt();
+            NextCard = deck.DealCard();
+            String result;
+            DisplayNextCard(NextCard);
+
+            if ((CurrentCard.GetValue() < NextCard.GetValue() && ans == 'h') || (CurrentCard.GetValue() > NextCard.GetValue() && ans == 'l')) {
+                CorretGuesses++;
+                result = "Your prediction was correct, You made " + CorretGuesses + " correct guesses";
+                DisplayResult(result);
+            } else {
+                result = "Your prediction was incorrect, You made " + CorretGuesses + " correct guesses";
+                DisplayResult(result);
+                playing = false;
+            }
+            CurrentCard = NextCard;
+        }
+        return CorretGuesses;
+    }
+
+    protected char GuessPrompt() {
+        boolean NoInput = true;
+        char answer = 'n';
+        while (NoInput) {
+            String guess = null;
+            guess = JOptionPane.showInputDialog(f, "Will the next card be higher (H) or lower (L)?", guess);
+            if ((guess.equals("h")) || (guess.equals("l"))) {
+                answer = guess.charAt(0);
+                NoInput = false;
+            } else {
+                JOptionPane.showMessageDialog(f, "Please input 'h' for HIGHER or 'l' for LOWER.");
+            }
+        }
+        return answer;
+    }
+
+    protected char PlayAgainPromt() {
+        char answer = 'n';
+        UIManager.put("OptionPane.cancelButtonText", "No");
+        UIManager.put("OptionPane.okButtonText", "Yes");
+        int ans = JOptionPane.showConfirmDialog(f, "Would you like to play again?", "Play Again?",
+                JOptionPane.YES_NO_OPTION);
+
+        if (ans == JOptionPane.YES_OPTION) {
+            answer = 'y';
+        }
+        if (ans == JOptionPane.NO_OPTION) {
+            answer = 'n';
+        }
+        UIManager.put("OptionPane.cancelButtonText", "Cancel");
+        UIManager.put("OptionPane.okButtonText", "OK");
+        return answer;
+    }
+    protected void DisplayFinalStats(){
+        JOptionPane.showMessageDialog(f, "You played a total of " + m_gamesPlayed + " games\n and socred a total of " + m_sumOfScores + " points \n for an overall avg score per game of " + (m_sumOfScores / m_gamesPlayed));
+    }
+
+    protected void DisplayCurrentCard(Card CurrentCard) {
+        JOptionPane.showMessageDialog(f, "The current card is " + CurrentCard);
+    }
+
+    protected void DisplayNextCard(Card NextCard) {
+        JOptionPane.showMessageDialog(f, "The Next card is " + NextCard);
+    }
+
+    protected void DisplayResult(String result) {
+        JOptionPane.showMessageDialog(f, result);
+    }
+}
